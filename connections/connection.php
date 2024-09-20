17f7e07a9a11725fc3b2223097d8c19169ecc27e
Lasindu Themiya <lasinduthemiya96@gmail.com>
2024-09-20 13:17:57 +0530

    Connection to the database

---
+++ connections/connection.php
@@ -0,0 +1,4 @@
+<?php
+$con = mysqli_connect("fdb1030.awardspace.net","4514420_queuepro","Marvel(9)","4514420_queuepro");
+
+?>
---
+++ connections/connectionpdo.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = 'fdb1030.awardspace.net';
+$db   = '4514420_queuepro';
+$user = '4514420_queuepro';
+$pass = 'Marvel(9)';
+$charset = 'utf8mb4';
+
+
+?>
